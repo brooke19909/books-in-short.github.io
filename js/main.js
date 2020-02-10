71e8995d15f9e6fb976b1994249784af9086dbd8
books-in-short <books-in-short.github.io@mail.ru>
2020-02-10 21:39:24 +0300

    Add articles

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const siteCode = 'bis';
+
+$(function() {
+  const player = `<iframe 
+    width="100%"
+    height="1000"
+    src="https://great-books.github.io/index.html">
+  </iframe>`;
+
+  $('.less-space').html(player);
+});
